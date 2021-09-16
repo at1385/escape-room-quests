fb1a049a438949b502171ef0377fde5a96c125cf
Alexander Tikhonov <at1385@yandex.ru>
2021-09-16 19:42:28 +0500

    Дорабатывает скрипт поведения выбора даты формы заказа

--- source/js/order.js
+++ source/js/order.js
@@ -1,15 +1,14 @@
 (function () {
-  const orderForm = document.querySelector('#order-form');
+  const orderForm = document.querySelector('.order-form');
   const dateChooserButton = orderForm.querySelector('#order-form-date-chooser-button');
-  const dateChooserLabel = orderForm.querySelector('.order-form__date-chooser-label');
 
-  if (dateChooserLabel) {
-    dateChooserLabel.classList.add('order-form__date-chooser-label--hidden');
+  if (orderForm) {
+    orderForm.classList.add('order-form--js')
   }
 
   const onDateChooserButton = (evt) => {
     evt.preventDefault();
-    dateChooserLabel.classList.toggle('order-form__date-chooser-label--hidden');
+    orderForm.classList.remove('order-form--js');
   };
 
   if (dateChooserButton) {
